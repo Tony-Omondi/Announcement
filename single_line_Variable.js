6bd8299fb70844d38febb7cde5a06acedd8ae03a
Tony-Otieno <nitonito598@gmail.com>
2024-08-12 12:48:42 +0300

    announcments assigment

---
+++ single_line_Variable.js
@@ -0,0 +1,12 @@
+//On one single line, declare three variables with the following names and values:
+
+//firstName = "John"
+//lastName = "Doe"
+//age = 35
+
+let firstName = "John", lastName = "Doe", age = 35;
+
+console.log('firstName:',firstName);
+console.log('LastName:',lastName);
+console.log('Age:',age);
+
---
+++ variable_Z.js
@@ -0,0 +1,4 @@
+//Create a variable called z, assign x + y to it, and display the result in a console function.
+
+let z = x + y;
+console.log(z);
